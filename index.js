3637479fddadcf8f7ebeee2ebd2cc602d2070249
yawkar <yawkarpov@gmail.com>
2023-03-03 17:30:40 +0300

    chore: add license & rename some misspellings

---
+++ index.js
@@ -0,0 +1,3 @@
+
+
+console.log('Nothing to see here ^_^');
--- manualStoopidTests.js
+++ manualStoopidTests.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const SegmentTree = require('./SegmentTree');
-const SummerTreeFuncs = require('./TreesTypes/Summer.js');
+const SummerTreeType = require('./TreesTypes/Summer.js');
 
 let arr = [1, 2, 3, 4, 5];
 
-let segTree = new SegmentTree(arr, SummerTreeFuncs.mergeFunction, SummerTreeFuncs.nodeFactory);
+let segTree = new SegmentTree(arr, SummerTreeType.mergeFunction, SummerTreeType.nodeFactory);
 
 segTree.extract(0, 5, resultNode => {
     console.log(resultNode)
